b353039649b2eeb740b6238c209cf5cc4c2e75f1
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-02-01 22:50:25 +0530

    updated code for concatenation and type coercion of truthy and falsy values in JS

--- JS/Basics/3_concateAndCoercion.js
+++ JS/Basics/3_concateAndCoercion.js
@@ -2,3 +2,7 @@ let sum = "Rohit" + 5;
 console.log(sum);
 console.log(typeof sum);
 console.log(false - true);
+console.log(true + false);
+console.log(true + true);
+console.log(false + false);
+console.log(true - false);
