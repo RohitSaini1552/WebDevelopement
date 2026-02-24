4ebbc325989edaaaaf6035a6fdc7a452cb806430
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-02-24 18:32:44 +0530

    addd code for displaying the output area of shape using switch statement

--- JS/Basics/5_switchStatement.js
+++ JS/Basics/5_switchStatement.js
@@ -1,29 +1,57 @@
 const prompt = require('prompt-sync')();
 
-const day = prompt('Enter your day: ');
+// Code 1: Week Days
+// const day = prompt('Enter your day: ');
 
-switch(day){
-    case "Monday":
-        console.log("today is monday");
+// switch(day){
+//     case "Monday":
+//         console.log("today is monday");
+//         break;
+//         case "Monday":
+//         console.log("today is monday");
+//         break;
+//         case "Tuesday":
+//         console.log("today is Tuesday");
+//         break;
+//         case "Wednesday":
+//         console.log("today is Wednesday");
+//         break;
+//         case "Thursday":
+//         console.log("today is Thursday");
+//         break;
+//         case "Friday":
+//         console.log("today is Friday");
+//         break;
+//         case "Saturday":
+//         console.log("today is Saturday");
+//         break;
+//         default:
+//             console.log("Sunday is the Funday");
+// }
+
+//code 2:
+const shape = prompt('Enter the shape: ');
+const r = 7;
+const l = 2;
+const b = 3;
+const s = 4;
+var area;
+
+switch(shape){
+    case "circle":
+        area = 3.14 * r * r;
+        console.log("Area of circle :",area);
         break;
-        case "Monday":
-        console.log("today is monday");
+    case "square":
+        area = s * s;
+        console.log("Area of square :",area);
         break;
-        case "Tuesday":
-        console.log("today is Tuesday");
-        break;
-        case "Wednesday":
-        console.log("today is Wednesday");
-        break;
-        case "Thursday":
-        console.log("today is Thursday");
-        break;
-        case "Friday":
-        console.log("today is Friday");
-        break;
-        case "Saturday":
-        console.log("today is Saturday");
+    case "rectangle":
+        area = l * b;
+        console.log("Area of ectangle :",area);
         break;
         default:
-            console.log("Sunday is the Funday");
+            console.log("Invalid Shape");
 }
+
+
