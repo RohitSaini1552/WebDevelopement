079cdbb875d5e6093dde44ae9654f7e08915aea3
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-02-24 18:24:34 +0530

    add code to classify week days using switch statements

---
+++ JS/Basics/5_switchStatement.js
@@ -0,0 +1,29 @@
+const prompt = require('prompt-sync')();
+
+const day = prompt('Enter your day: ');
+
+switch(day){
+    case "Monday":
+        console.log("today is monday");
+        break;
+        case "Monday":
+        console.log("today is monday");
+        break;
+        case "Tuesday":
+        console.log("today is Tuesday");
+        break;
+        case "Wednesday":
+        console.log("today is Wednesday");
+        break;
+        case "Thursday":
+        console.log("today is Thursday");
+        break;
+        case "Friday":
+        console.log("today is Friday");
+        break;
+        case "Saturday":
+        console.log("today is Saturday");
+        break;
+        default:
+            console.log("Sunday is the Funday");
+}
