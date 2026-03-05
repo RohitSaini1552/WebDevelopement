e6602b7e3212fd3ae7e30a4928eb545e14fd945a
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-03-05 10:52:47 +0530

    add code to print numbers from 1 to 10 using while loop

---
+++ JS/Basics/6_whileLoop.js
@@ -0,0 +1,22 @@
+const prompt = require('prompt-sync')();
+// const shape = prompt('Enter the shape: ');
+
+
+
+// Q1 - write a code to print numbers from 1 to 10 using a while loop
+
+// var i = 1;
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+
+//Q2 - write a code to print the table of 5 using while loop
+
+var i = 1;
+while(i <= 10){
+    // console.log("5 * ",i," = ",5 * i);
+    console.log(`5 * ${i} = ${5 * i}`);
+    i++;
+}
