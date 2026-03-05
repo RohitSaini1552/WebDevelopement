f398023699e7a60c4756cfd255667f20bad2f09a
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-03-05 11:05:40 +0530

    add code to implement number checking logic that check userinput until it is a positive nymber

--- JS/Basics/6_whileLoop.js
+++ JS/Basics/6_whileLoop.js
@@ -5,19 +5,33 @@ const prompt = require('prompt-sync')();
 
 // Q1 - write a code to print numbers from 1 to 10 using a while loop
 
-var i = 1;
-while(i <= 10){
-    console.log(i);
-    i++;
-}
+// var i = 1;
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
 
 //Q2 - write a code to print the table of 5 using while loop
 
-var i = 1;
-while(i <= 10){
-    // console.log("5 * ",i," = ",5 * i);
-    console.log(`5 * ${i} = ${5 * i}`);
-    i++;
+// var i = 1;
+// while(i <= 10){
+//     // console.log("5 * ",i," = ",5 * i);
+
+//     //using template literals
+//     console.log(`5 * ${i} = ${5 * i}`);
+//     i++;
+// }
+
+//Q3 - check whether user gives correct input(+ve number)
+
+let userInput = parseInt(prompt('Enter the Input: '));
+
+while(userInput < 0){
+    userInput = parseInt(prompt('Enter the Input again: '));
 }
+
+console.log("Congratulations you made a Correct number input");
+
+
 
 
