f6e228d27b225bfbb0f433672a3597e4041d402a
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-03-20 21:24:31 +0530

    add code to reverse a string

---
+++ JS/Function/4_reveseString.js
@@ -0,0 +1,9 @@
+let reverse = (str) => {
+    let result = "";
+    for(let char=str.length-1;char>=0;char--){
+        result += str[char];
+    }
+    return result;
+};
+
+console.log(reverse("Master"));
