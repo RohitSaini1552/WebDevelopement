1a931d94da203eb2d1f6f31695f90cc5b732f0dd
RohitSaini1552 <rohitsaini1552@gmail.com>
2026-04-24 18:30:36 +0530

    add code to print array multilpy by 2 using map method in arrays

--- JS/Arrays/1_multiplyEachElementBy2.js
+++ JS/Arrays/1_multiplyEachElementBy2.js
@@ -2,6 +2,17 @@
 
 let arr = [1,2,3,4,5,6,7];
 
+/* using for each loop 
+
 let multiplyBy2 = arr.forEach((currElem) => {
-    console.log(currElem * 2);
+    currElem *= 2;
+    console.log(currElem);
+}); */
+
+// using map method 
+let multiplyBy2 = arr.map((currElem) => {
+    return currElem * 2;
 });
+console.log(multiplyBy2);
+
+console.log(arr);
